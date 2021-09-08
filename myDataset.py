1a1e3ea95e53da0ca69379ab4f19730f9648f11d
Liu Yang <1369939686@qq.com>
2021-09-08 09:12:34 +0800

    Add the code for the creation of a Custom Dataset

---
+++ myDataset.py
@@ -0,0 +1,32 @@
+import os
+import pandas as pd
+from torch.utils.data import Dataset
+from torchvision.io import read_image
+
+
+# Used to store the dataset in a directory img_dir
+# Labels are stored in a CSV file annotations_file
+
+class CustomImageDataset(Dataset):
+    # The __init__ function is run once when instantiating the Dataset object.
+    def __init__(self, annotations_file, img_dir, transform=None, target_transform=None):
+        self.img_labels = pd.read_csv(annotations_file)  # Store the data in a DataFrame
+        self.img_dir = img_dir
+        self.transform = transform
+        self.target_transform = target_transform
+
+    # The __len__ function returns the number of samples in our dataset.
+    def __len__(self):
+        return len(self.img_labels)
+
+    # The __getitem__ function loads and returns a sample from the dataset at the given index idx
+    # Including image and the corresponding label
+    def __getitem__(self, idx):
+        img_path = os.path.join(self.img_dir, self.img_labels.iloc[idx, 0])  # ???
+        image = read_image(img_path)  # read image and transfer it to a tensor
+        label = self.img_labels.iloc(idx, 1)  # retrieve the corresponding label
+        if self.transform:
+            image = self.transform(image)
+        if self.target_transform:
+            image = self.target_transform(label)
+        return image, label
--- showData.py
+++ showData.py
@@ -1,5 +1,5 @@
 import torch
-from torch.utils.data import Dataset
+from torch.utils.data import DataLoader
 from torchvision import datasets
 from torchvision.transforms import ToTensor
 import matplotlib.pyplot as plt
@@ -40,3 +40,16 @@ for i in range(1, rows * cols + 1):
     plt.axis("off")
     plt.imshow(img.squeeze(), cmap="gray")  # squeeze()去除tensor中为1的维度，把img变成二维；cmap控制颜色图谱
 plt.show()
+
+train_dataloader = DataLoader(training_data, batch_size=64, shuffle=True)
+test_dataloader = DataLoader(test_data, batch_size=64, shuffle=True)
+
+# Display image and label
+train_features, train_labels = next(iter(train_dataloader))
+print(f"Feature batch shape: {train_features.size()}")
+print(f"Labels batch shape: {train_labels.size()}")
+img = train_features[0].squeeze()
+label = train_labels[0]
+plt.imshow(img, cmap="gray")
+plt.show()
+print(f"Label: {label}")
